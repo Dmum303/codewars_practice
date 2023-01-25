b3822f91500b81188962f0f01c893a94cf89801e
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-25 15:58:12 +0000

    solved persistence problem

--- lib/persistence.rb
+++ lib/persistence.rb
@@ -1,1 +1,9 @@
-
+def persistence(n)
+  count = 0
+  return 0 if n.to_s.length == 1
+  while n.to_s.length > 1
+    n = n.to_s.chars.map(&:to_i).reduce(:*)
+    count += 1
+  end
+  return count
+end
--- spec/persistence_spec.rb
+++ spec/persistence_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe "Persistence" do
   it "tests the truth" do
     expect(true).to eq true
   end
+
+  it "returns 0 for 4" do
+    expect(persistence(4)).to eq 0
+  end
+
+  it "returns 3 for 39" do
+    expect(persistence(39)).to eq 3
+  end
+
+  it "returns 4 for 999" do
+    expect(persistence(999)).to eq 4
+  end
 end
