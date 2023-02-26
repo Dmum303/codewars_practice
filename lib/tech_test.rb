d8d48afde969461bc88e5a6a62a21b8d3571bb09
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-02-26 17:15:58 +0000

    codily test

--- lib/tech_test.rb
+++ lib/tech_test.rb
@@ -1,16 +1,85 @@
-def diagonalDifference(arr)
-    left = []
-    right = []
-    n = arr.length
-    (0...n).each do |i|
-        left << arr[i][i]
-        right << arr[i][-(i+1)]
-    end
-    (left.sum - right.sum).abs
-end
+# def diagonalDifference(arr)
+#     left = []
+#     right = []
+#     n = arr.length
+#     (0...n).each do |i|
+#         left << arr[i][i]
+#         right << arr[i][-(i+1)]
+#     end
+#     (left.sum - right.sum).abs
+# end
 
-def countingSort(arr)
-    sorted = arr.sort.reverse
-    sorted[1]
-    arr[0]
+# def countingSort(arr)
+#     new_arr = (0..arr.max()).map {|i| 0}
+#     arr.each {|j| new_arr[j] += 1}
+#     new_arr
+# end
+
+# def solution(a)
+#   patient_hash = Hash.new(0)
+#   a.each do |patient|
+#     patient_hash[patient] += 1
+#   end
+#   patient_hash.each do |key, value|
+#     # puts key
+#     # puts value
+#   end
+#   patient_hash.values.sort[-1]
+# end
+
+#   def solution_2(string)
+#   operations = string.split(" ")
+#   stack = []
+#   operations.each {|op|
+#     if op == "POP"
+
+#     elsif op == "DUP"
+
+#     elsif op == "+"
+#       first = stack[-2]
+#       second = stack[-1]
+#       result = first + second
+#       stack.pop(2)
+#       stack << result
+#     elsif op == "-"
+#       first = stack[-2]
+#       second = stack[-1]
+#       result = second - first
+#       stack.pop(2)
+#       stack << result
+#     else
+#       stack << op.to_i
+#     end
+#   }
+#     stack[-1]
+#   end
+
+# end
+
+def solution_3(a, x)
+    # n = a.length
+    # if (n == 0) then
+    #     return -1
+    # end
+    # l = 0
+    # r = n - 1
+    # while (l < r)
+    #     m = (l + r) / 2
+    #     if (a[l] != x) then
+    #         l += 1
+    #     else
+    #         # binding.irb
+    #         return a.index(x)
+    #     end
+    # end
+    # # binding.irb
+    # if (a[l] == x) then
+    #     return l
+    # end
+    # return -1
+    if a.include? x
+        a.index(x)
+    else
+        -1
+    end
 end
--- spec/tech_test_spec.rb
+++ spec/tech_test_spec.rb
@@ -1,17 +1,27 @@
 require_relative "../lib/tech_test"
 
 RSpec.describe "a specification" do
-  it "should pass" do
-    expect(true).to eq(true)
-  end
+  # it "should pass" do
+  #   expect(true).to eq(true)
+  # end
 
-  it "tests diagonal problem" do
-    expect(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]])).to eq(15)
-  end
+  # it "tests diagonal problem" do
+  #   expect(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]])).to eq(15)
+  # end
 
-  it "tests comparison problem" do
-    expect(countingSort([1,1,3,2,1])).to eq [0, 3, 1, 1]
-  end
+  # it "tests comparison problem" do
+  #   expect(countingSort([1,1,3,2,1])).to eq [0, 3, 1, 1]
+  # end
 
+  # it "tests comparison problem" do
+  #   expect(solution(['Cardiology', 'Orthopaedics', 'Neurology', 'Cardiology', 'Orthopaedics', 'Cardiology'])).to eq 3
+  # end
+
+  # it "tests operation array" do
+  #   expect(solution_2('4 5 6 - 7 +')).to eq 8
+  # end
+  it "tests operation array" do
+    expect(solution_3([1, 2, 5, 9, 9], 5)).to eq 2
+  end
 
 end
