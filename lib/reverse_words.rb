71450f7955d753292773b02c9017566e14cc6612
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-03-23 10:02:24 +0000

    refactored

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,5 +1,4 @@
 def reverser(string)
-  string_array = string.split(" ")
-  string_array.each { |word| string = string.gsub(word, word.reverse) }
+  string.split(" ").each { |word| string = string.gsub(word, word.reverse) }
   string
 end
