307a7cc40b3de1d5080355c086de99acf28674ac
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-04-18 10:10:46 +0100

    Continue with level 4 problem

--- lib/human_readable_duration_format.rb
+++ lib/human_readable_duration_format.rb
@@ -1,23 +1,37 @@
 def format_duraction(seconds)
   return "now" if seconds == 0
   result_array = []
-  if seconds > 60
-    minutes = seconds / 60
-    seconds = seconds % 60
-    if minutes == 1
-      result_array << "#{minutes} minute"
-    elsif minutes > 1
-      result_array << "#{minutes} minutes"
-    end
-    if seconds == 1
-      result_array << "#{seconds} second"
-    elsif seconds > 1
-      result_array << "#{seconds} seconds"
-    end
+  hours = seconds / 3600
+  minutes = ((seconds / 60) % 60)
+  seconds = seconds % 60
+  if hours == 1
+    result_array << "#{hours} hour"
+  elsif hours > 1
+    result_array << "#{hours} hours"
   end
-  result_array.length > 1 ? result_array.join(" and ") : result_array[0]
+  if minutes == 1
+    result_array << "#{minutes} minute"
+  elsif minutes > 1
+    result_array << "#{minutes} minutes"
+  end
+  if seconds == 1
+    result_array << "#{seconds} second"
+  elsif seconds > 1
+    result_array << "#{seconds} seconds"
+  end
+  if result_array.length > 2
+    joined = result_array[0..-2].join(", ")
+    joined.split(" ").push("and").push(result_array[-1]).join(" ")
+  elsif result_array.length == 2
+    result_array.insert(-2, "and")
+    result_array.join(" ")
+  else
+    result_array[0]
+  end
 end
 
 # Could work out seconds, minutes, hours, days, weeks, months, years
 #  Stick all the ones with > 0 into an array and if == 0 return. If greater than 1
 #  add "and" for [-1]
+# Now having the issue of commas and "and" in the right place, I imagine that a way of doing this would be to to take a slice of n-1 and join them with commas, then split back to an array and add the last element with "and" in front of it. That worked well
+#  Next add days and years
--- spec/human_readable_duraction_format_spec.rb
+++ spec/human_readable_duraction_format_spec.rb
@@ -1,19 +1,39 @@
 require_relative "../lib/human_readable_duration_format"
 
 RSpec.describe "HumanReadableDurationFormat" do
-  it "returns the correct duration" do
+  it "returns the correct duration 1" do
     expect(format_duraction(1)).to eq "1 second"
   end
 
-  it "returns the correct duration" do
+  it "returns the correct duration 2" do
     expect(format_duraction(2)).to eq "2 seconds"
   end
 
-  it "returns the correct duration" do
+  it "returns the correct duration 0" do
     expect(format_duraction(0)).to eq "now"
   end
 
-  it "returns the correct duration" do
+  it "returns the correct duration 62" do
     expect(format_duraction(62)).to eq "1 minute and 2 seconds"
+  end
+
+  it "returns the correct duration 60" do
+    expect(format_duraction(60)).to eq "1 minute"
+  end
+
+  it "returns the correct duration 120" do
+    expect(format_duraction(120)).to eq "2 minutes"
+  end
+
+  it "returns the correct duration 127" do
+    expect(format_duraction(127)).to eq "2 minutes and 7 seconds"
+  end
+
+  it "returns the correct duration 3600" do
+    expect(format_duraction(3600)).to eq "1 hour"
+  end
+
+  it "returns the correct duration 3600" do
+    expect(format_duraction(3662)).to eq "1 hour, 1 minute and 2 seconds"
   end
 end
