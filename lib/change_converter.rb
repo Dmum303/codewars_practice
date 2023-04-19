8cd047eaa13ee993593ade1e1b3ed7bba922fef3
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-04-19 17:02:33 +0100

    change converter second last test failing

--- lib/change_converter.rb
+++ lib/change_converter.rb
@@ -3,7 +3,20 @@ class ChangeConverter
   end
 
   # Hash should have highest value first
-  Denomination = { "10p" => 10, "5p" => 5, "2p" => 2, "1p" => 1 }
+  Denomination = {
+    "£50" => 5000,
+    "£20" => 2000,
+    "£10" => 1000,
+    "£5" => 500,
+    "£2" => 200,
+    "£1" => 100,
+    "50p" => 50,
+    "20p" => 20,
+    "10p" => 10,
+    "5p" => 5,
+    "2p" => 2,
+    "1p" => 1
+  }
 
   def self.convert(amount)
     converted = []
--- spec/change_converter_spec.rb
+++ spec/change_converter_spec.rb
@@ -24,6 +24,65 @@ RSpec.describe "ChangeConverter" do
   it "converts 0.10 to correct" do
     expect(ChangeConverter.convert(0.10)).to eq %w[10p]
   end
+
+  it "converts 0.20 to correct" do
+    expect(ChangeConverter.convert(0.20)).to eq %w[20p]
+  end
+
+  it "converts 0.50 to correct" do
+    expect(ChangeConverter.convert(0.50)).to eq %w[50p]
+  end
+
+  it "converts 1 to correct" do
+    expect(ChangeConverter.convert(1)).to eq %w[£1]
+  end
+
+  it "converts 2 to correct" do
+    expect(ChangeConverter.convert(2)).to eq %w[£2]
+  end
+
+  it "converts 5 to correct" do
+    expect(ChangeConverter.convert(5)).to eq %w[£5]
+  end
+
+  it "converts 10 to correct" do
+    expect(ChangeConverter.convert(10)).to eq %w[£10]
+  end
+
+  it "converts 20 to correct" do
+    expect(ChangeConverter.convert(20)).to eq %w[£20]
+  end
+
+  it "converts 50 to correct" do
+    expect(ChangeConverter.convert(50)).to eq %w[£50]
+  end
+
+  it "converts 6.42 to correct" do
+    expect(ChangeConverter.convert(6.42)).to eq %w[£5 £1 20p 20p 2p]
+  end
+
+  it "converts 40 to correct" do
+    expect(ChangeConverter.convert(40)).to eq %w[£20 £20]
+  end
+
+  it "converts 16.30 to correct" do
+    expect(ChangeConverter.convert(16.30)).to eq %w[£10 £5 £1 20p 10p]
+  end
+
+  it "converts 19.99 to correct change" do
+    expect(ChangeConverter.convert(19.99)).to eq %w[
+         £10
+         £5
+         £2
+         £2
+         50p
+         20p
+         20p
+         5p
+         2p
+         2p
+       ]
+  end
 end
 
 # Acceptance Criteria
